d5b05b3e9660ec330f508d17049e5fb722873dc7
Ahad Chowdhury <ahad@maacpiash.com>
2024-12-28 16:42:47 +1100

    ✨ Add basic logic for redirection via KV lookup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Env = {
+	Bindings: {
+		KV_NS: KVNamespace
+	}
+}
 
-app.get('/', c => {
-	return c.json('Hello Hono!')
+const app = new Hono<Env>()
+
+app.get('/:key', async ({ env, req, status, redirect }) => {
+	try {
+		const url = await env.KV_NS.get(req.param('key'))
+		if (url === null) return status(404)
+		return redirect(url)
+	} catch (e) {
+		console.error(e)
+		return status(500)
+	}
 })
 
 export default app
