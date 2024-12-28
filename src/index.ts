14918ce5abafc2f1193c959827e2b6a2a6581b1e
Ahad Chowdhury <ahad@maacpiash.com>
2024-12-28 16:31:10 +1100

    🎨 Add Prettier and ESLint to format and lint code

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.json('Hello Hono!')
+app.get('/', c => {
+	return c.json('Hello Hono!')
 })
 
 export default app
